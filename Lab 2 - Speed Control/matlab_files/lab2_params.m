4f73ff5d6d1292961497545d352041f65a39b29c
Michael Giglia <michael.giglia@cooper.edu>
2022-09-23 13:55:47 -0400

    first commit 9-23-2022, fixed lab2 template graphing sizes

---
+++ Lab 2 - Speed Control/matlab_files/lab2_params.m
@@ -0,0 +1,10 @@
+% This .m files holds the related parameters for the 
+% open_loop_voltage simulink template file.
+% Run this file so that all the appropriate values are in the workspace.
+
+
+V = 6       % The voltage we want to apply to the motor in the open loop
+K = 26.4    % The gain of the motor voltage -> angular_velocity
+tau = 0.158   % The time constant of the model
+
+w = 50
